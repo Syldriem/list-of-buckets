9e36da3a6970a5251d686e69669643ef33819d9d
Syldriem <93327634+Syldriem@users.noreply.github.com>
2024-07-04 15:19:45 +0200

    1 & 2

---
+++ index.js
@@ -0,0 +1,6 @@
+const item = document.createElement("p");
+item.innerText = "Travel to _";
+document.body.appendChild(item);
+const item2 = document.createElement("p");
+item2.innerText = "Do _";
+document.body.insertAdjacentElement("beforeend", item2);
