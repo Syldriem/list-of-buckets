e6cd10eeba62379eb13bde9637f85eff282db565
Syldriem <93327634+Syldriem@users.noreply.github.com>
2024-07-04 16:18:10 +0200

    3-8

--- index.js
+++ index.js
@@ -4,3 +4,16 @@ document.body.appendChild(item);
 const item2 = document.createElement("p");
 item2.innerText = "Do _";
 document.body.insertAdjacentElement("beforeend", item2);
+const list = document.querySelector(".list");
+console.log(list.innerHTML);
+const newItem = "<div>Eat _</div>";
+//document.querySelector(".list").innerHTML = newItem;
+list.insertAdjacentHTML("afterbegin", newItem);
+const childlist = list.children;
+const item3 = document.createElement("p");
+
+for (i = 0; i < 3; i++) {
+	list.insertAdjacentHTML("beforeend", newItem);
+}
+
+console.log(list.children.length)
